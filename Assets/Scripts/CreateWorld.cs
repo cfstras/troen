21f1539142e0bb34fb1217fea8a2fe263a2fb9cd
unknown <magerp@GEPRAKTIKUM01.ad.rbg.tum.de>
2012-12-19 14:49:59 +0100

    world gets created

--- Assets/Scripts/CreateWorld.cs
+++ Assets/Scripts/CreateWorld.cs
@@ -20,7 +20,21 @@ public class CreateWorld : MonoBehaviour {
 		planeFront 	= (GameObject) Instantiate(prefabPlane);
 		planeBack  	= (GameObject) Instantiate(prefabPlane);
 		
-		planeBack.transform.rotation = Quaternion.Euler(90,0,0);
+		planeFloor.name = "planeFloor";
+		planeLeft.name = "planeLeft";
+		planeRight.name = "planeRight";
+		planeFront.name = "planeFront";
+		planeBack.name = "planeBack";
 		
+		planeBack.transform.rotation = Quaternion.Euler(90,0,0);
+		planeFront.transform.rotation = Quaternion.Euler(-90,0,0);
+		planeLeft.transform.rotation = Quaternion.Euler(-90,0,90);
+		planeRight.transform.rotation = Quaternion.Euler(90,0,-90);
+		
+		planeBack.transform.position = new Vector3(0,0,-5f);
+		planeFront.transform.position = new Vector3(0,0,5f);
+		planeLeft.transform.position = new Vector3(5,0,0);
+		planeRight.transform.position = new Vector3(-5,0,0);
+		planeFloor.transform.position = new Vector3(0,-5,0);
 	}
 }
